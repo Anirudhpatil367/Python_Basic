50c2e8baca2f5ea5c27a63608760a7a17b1094f8
Anirudh <Anirudhpatil367@gmail.com>
2024-10-05 00:21:27 -0500

    day_1.1

--- day_1.py
+++ day_1.py
@@ -14,20 +14,80 @@
 #False
 
 #print()
-print('HEllo world!')
-print(4.5)
-print('4.5','Hello"')
-print("hello","end",87,False, end='|')
-print("hello")
+#print('HEllo world!')
+#rint(4.5)
+#print('4.5','Hello"')
+#print("hello","end",87,False, end='|')
+#print("hello")
 
 #Variables
-hello='tim'
-world= 'world'
-world =hello
-hello='no'
-print(hello,world)
+#hello='tim'
+#world= 'world'
+#world =hello
+#hello='no'
+#print(hello,world)
 
 #naming convention
 #hello_world 
 #not posssible -9helloworld
 #possible --> hello_world_32
+
+#input('Name: ')
+#name=input
+#print(name)
+
+#name=input('Name: ')
+#age= input('age: ')
+#print('Hello',name, 'you are ',age, "years old ")
+
+#arthemetic operators 
+x=9
+y=3
+result=x+y
+print(result)
+#subtraction
+x=9
+y=3
+result=x-y
+print(result)
+#multiplication
+x=9
+y=3
+result=x*y
+print(result)
+#division
+x=9
+y=3
+result=x/y
+print(result)
+#power
+x=9
+y=3
+result=x**y
+print(result)
+#additon_1
+x=9
+y=3
+result=int(x/y)
+print(result)
+#Quotient
+x=9
+y=3
+result=x//y
+print(result)
+#mod(Reminder)
+x=9
+y=3
+result=float(x%y)
+print(result)
+#python follows BEDMAS(Bracket,Exponents,division,multiplication,addition,subtraction)
+x=9
+y=3
+result=float(x%y)*2
+print(result)
+#---
+#num=input('Number: ')#-->STR
+#print(num -5)
+#---
+num=input('Number: ')#-->STR
+print(int(num) -5)
