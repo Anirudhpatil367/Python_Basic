3848c11324a0db5ce83afaba2c8051364e847230
Anirudh <Anirudhpatil367@gmail.com>
2024-10-04 23:23:43 -0500

    Day_1 commit

---
+++ day_1.py
@@ -0,0 +1,33 @@
+# Data types 
+
+#int
+#-267236
+#76373
+#float
+#2727.0
+#-9.7
+#string
+#'hello"'
+#"hello"
+#bool
+#True 
+#False
+
+#print()
+print('HEllo world!')
+print(4.5)
+print('4.5','Hello"')
+print("hello","end",87,False, end='|')
+print("hello")
+
+#Variables
+hello='tim'
+world= 'world'
+world =hello
+hello='no'
+print(hello,world)
+
+#naming convention
+#hello_world 
+#not posssible -9helloworld
+#possible --> hello_world_32
