23b2a5144bc3bf018c7fade4a70b8460508759e5
Anirudh <Anirudhpatil367@gmail.com>
2024-10-06 01:32:16 -0500

    Day_2 commit

---
+++ day_2.py
@@ -0,0 +1,24 @@
+#num=input('Number: ')
+#print(float(num)-5)
+
+#hello='hello'
+#print(type(hello))
+
+#string method : (something with dot operator)
+hello='hello'.upper()
+print(hello)
+#or 
+hello='hello'
+print(hello.upper())
+#lower
+hello='hello'
+print(hello.lower())
+#capitallize
+hello='hello'
+print(hello.capitalize())
+#string in another string
+hello='hello'
+print(hello.count('ll'))
+#using two string 
+hello='hello'
+print(hello.lower().count('ll'))
