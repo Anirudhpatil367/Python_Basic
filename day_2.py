36437726c53fe6ad277cec6c15ffa0046ff81cbf
Anirudh <Anirudhpatil367@gmail.com>
2024-10-06 02:06:10 -0500

    Day_2.1 commit

--- day_2.py
+++ day_2.py
@@ -3,22 +3,91 @@
 
 #hello='hello'
 #print(type(hello))
-
+#
 #string method : (something with dot operator)
-hello='hello'.upper()
-print(hello)
+#hello='hello'.upper()
+#print(hello)
 #or 
-hello='hello'
-print(hello.upper())
+#hello='hello'
+#print(hello.upper())
 #lower
-hello='hello'
-print(hello.lower())
+#hello='hello'
+#print(hello.lower())
 #capitallize
-hello='hello'
-print(hello.capitalize())
+#hello='hello'
+#print(hello.capitalize())
 #string in another string
-hello='hello'
-print(hello.count('ll'))
+#hello='hello'
+#print(hello.count('ll'))
 #using two string 
-hello='hello'
-print(hello.lower().count('ll'))
+#hello='hello'
+#print(hello.lower().count('ll'))
+
+#x='helo'
+#y=3
+#print(x*y)#it is like (hello x 3)
+
+#conditional operator
+#True 
+#False
+#==
+#!=
+#<=
+#>=
+x="hello"
+y="hello"
+print(x==y)
+#
+x="hello"
+y="hello1"
+print(x==y)
+#
+print('a'>'z')
+print(ord('a'))
+
+#
+print(7.5==7)
+print()
+#chained condition
+x=7
+y=8 
+z=0
+result1=x==y
+result2=y>x
+result3=z+x+2
+#and 
+#or 
+#not 
+result4=result1 or result2
+print(result4)
+
+#or 
+print(not (False or True))
+print(not(False and True))
+#order
+#not
+#and 
+#or
+
+#if/else/if-else
+x=input('Name: ')
+#if condition:
+if x=='Tim':
+    print("you are great")
+print("always fo this ")
+#if -else 
+if x=='Tim':
+    print("you are great")
+else:
+    print("no")
+print("always fo this ")
+#elif statement
+if x=='Tim':
+    print("you are great")
+elif x=='joe':
+    print("no")
+else:
+    print("always fo this ")
+print("default")
+
+#collections
